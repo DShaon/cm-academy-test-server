97b28f665af4af9a1d9341505f33cce655dbe6d2
DShaon <toxicshaon@gmail.com>
2023-08-25 17:03:02 +0600

    added blog

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { ObjectId } = require('mongodb'); // Import ObjectId
+
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -26,10 +28,29 @@ async function run() {
 
         const categoriesCollection = client.db('CM').collection('Categories');
         const usersCollection = client.db('CM').collection('users');
+        const blogCollection = client.db('CM').collection('Blog');
 
 
 
 
+
+
+
+        // Route to blog insertion
+        app.post('/blog', async (req, res) => {
+            try {
+                const blog = req.body; // Form data sent from the client
+
+                // Insert the form data into the MongoDB collection 'Categories'
+                const categoriesCollection = client.db('CM').collection('Blog');
+                await blogCollection.insertOne(blog);
+
+                res.status(201).json({ message: 'blog added successfully' });
+            } catch (error) {
+                console.error('Error Adding blog:', error);
+                res.status(500).json({ message: 'An error occurred', error: error.message });
+            }
+        });
 
         // Route to handle form data insertion
         app.post('/addCourse', async (req, res) => {
@@ -47,11 +68,41 @@ async function run() {
             }
         });
 
+        // Route to blog insertion
+        app.post('/blog', async (req, res) => {
+            try {
+                const blog = req.body; // blog data sent from the client
+                console.log(blog)
+
+                // Insert the blog data into the MongoDB collection 'Blog'
+                const blogCollection = client.db('CM').collection('Blog');
+                await blogCollection.insertOne(blog);
+
+                res.status(201).json({ message: 'blog added successfully' });
+            } catch (error) {
+                console.error('Error Adding blog:', error);
+                res.status(500).json({ message: 'An error occurred', error: error.message });
+            }
+        });
 
 
 
 
 
+        // Getting all blog from Db
+        app.get('/all-blog', async (req, res) => {
+            try {
+                const blogs = await blogCollection.find().toArray();
+                res.json(blogs);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Error fetching blog', error: error.message });
+            }
+        });
+
+
+
+        // Getting all courses categories from Db
         app.get('/categories', async (req, res) => {
             try {
                 const categories = await categoriesCollection.find().toArray();
@@ -61,6 +112,32 @@ async function run() {
                 res.status(500).json({ message: 'Error fetching categories', error: error.message });
             }
         });
+
+        
+        // Update the approval status of a course
+        app.put('/categories/:courseId/approval', async (req, res) => {
+            try {
+                const courseId = req.params.courseId;
+                const { ApprovedStatus } = req.body; // Use ApprovedStatus field
+
+                const result = await categoriesCollection.updateOne(
+                    { _id: new ObjectId(courseId) }, // Use new ObjectId() here
+                    { $set: { ApprovedStatus } } // Update the ApprovedStatus field
+                );
+
+                if (result.matchedCount === 0) {
+                    return res.status(404).json({ message: 'Course not found' });
+                }
+
+                res.json({ message: 'Course approval status updated successfully' });
+            } catch (error) {
+                console.error('Error updating course approval:', error);
+                res.status(500).json({ message: 'An error occurred', error: error.message });
+            }
+        });
+
+
+
 
         app.get('/categories/:categoryId/subCategories/:subCategoryId', async (req, res) => {
             try {
@@ -89,7 +166,6 @@ async function run() {
         });
 
 
-        console.log(process.env.DB_USER)
 
 
         app.get('/search', async (req, res) => {
@@ -114,7 +190,7 @@ async function run() {
         });
 
 
-
+        // Add all user into db
         app.post('/users', async (req, res) => {
             const user = req.body;
 
@@ -127,6 +203,8 @@ async function run() {
             res.send(result);
         });
 
+
+        // get user based user from db
         app.get('/users/:role', async (req, res) => {
             const role = req.params.role;
             try {
