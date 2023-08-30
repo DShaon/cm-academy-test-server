83947df5afff4a54dbbff5cee6bd81e0303ca454
DShaon <toxicshaon@gmail.com>
2023-08-30 20:50:54 +0600

    dfd

--- index.js
+++ index.js
@@ -290,8 +290,8 @@ async function run() {
                 total_amount: course?.coursePrice || "0",
                 currency: 'BDT',
                 tran_id: tran_id, // use unique tran_id for each api call
-                success_url: `https://cm-academy-test-server-production.up.railway.app/payment/success/${tran_id}`,
-                fail_url: `https://cm-academy-test-server-production.up.railway.app/fail/${tran_id}`,
+                success_url: `http://localhost:5000/payment/success/${tran_id}`,
+                fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
                 cancel_url: 'http://localhost:3030/cancel',
                 ipn_url: 'http://localhost:3030/ipn',
                 shipping_method: 'Courier',
